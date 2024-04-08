cd0094f2839eb9f4a6332414e790b9a3e5121d28
VLDLPY <vladlipay333@gmail.com>
2024-04-08 19:31:24 +0300

    main

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿// Плавный скролл к разделам 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+menuItems.forEach(item => {
+    item.addEventListener('click', function () {
+        const productId = item.dataset.productId;
+        const modalImg = document.querySelector('#productModal img');
+        modalImg.src = `images/product${productId}.jpg`; // Путь к изображениям продуктов
+        document.getElementById('productModal').style.display = 'block';
+    });
+});
+document.querySelectorAll('.close, #productModal').forEach(element => {
+    element.addEventListener('click', function () {
+        document.getElementById('productModal').style.display = 'none';
+    });
+});
